ebc6b0ff5dc9f88ea088cec90e44e393c1b77e07
Harry Mumford <harry_mumford@hotmail.co.uk>
2019-12-12 14:13:23 +0000

    refactor

--- src/interface.js
+++ src/interface.js
@@ -2,20 +2,28 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
 
+  
+
+  // $.getJSON('http://api.openweathermap.org/data/2.5/weather?id=2643743&APPID=0df10d735f6dd7a25a8e2ff3da09ee9e', function(data) {
+  //   var items = []
+    
+
+  // });
+
   $('#temperature-up').on('click', function() {
     thermostat.up();
     updateTemperature();
-  });
+  })
   
   $('#temperature-down').click(function() { 
     thermostat.down();
     updateTemperature();  
-  });
+  })
 
   $('#temperature-reset').click(function() {
     thermostat.resetTemperature();
     updateTemperature();
-  });
+  })
 
   $('#psm-on').click(function() {
     thermostat.switchPowerSavingModeOn();
@@ -32,6 +40,6 @@ $(document).ready(function() {
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
-  };
+  }
 });
 

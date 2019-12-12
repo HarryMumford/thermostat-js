cdf53e73e25eabd404adcb927defd5bc25635a15
Harry Mumford <harry_mumford@hotmail.co.uk>
2019-12-12 15:32:17 +0000

    get API temp data

--- src/interface.js
+++ src/interface.js
@@ -2,13 +2,10 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
 
-  
-
-  // $.getJSON('http://api.openweathermap.org/data/2.5/weather?id=2643743&APPID=0df10d735f6dd7a25a8e2ff3da09ee9e', function(data) {
-  //   var items = []
-    
-
-  // });
+  $.get('http://api.openweathermap.org/data/2.5/weather?id=2643743&APPID=0df10d735f6dd7a25a8e2ff3da09ee9e', function(data) {
+    var outsideTemp = Math.round(data.main.temp - 273.15)
+    $('#outside-temp').text(`Outside temperature: ${outsideTemp}°C`)
+  });
 
   $('#temperature-up').on('click', function() {
     thermostat.up();
@@ -27,13 +24,13 @@ $(document).ready(function() {
 
   $('#psm-on').click(function() {
     thermostat.switchPowerSavingModeOn();
-    $('#power-saving').text('on')
+    $('#power-saving-status').text('on')
     updateTemperature();
   })
 
   $('#psm-off').click(function() {
     thermostat.switchPowerSavingModeOff();
-    $('#power-saving').text('off')
+    $('#power-saving-status').text('off')
     updateTemperature();
   })
 

9f2ac2727a34ae9066252630e41a3b57276c0bb8
Harry Mumford <harry_mumford@hotmail.co.uk>
2019-12-12 08:37:30 +0000

    refactor

--- src/interface.js
+++ src/interface.js
@@ -31,6 +31,7 @@ $(document).ready(function() {
 
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
+    $('#temperature').attr('class', thermostat.energyUsage());
   };
 });
 

03187812ded90008f512ac00deff99f2b3f50c79
Harry Mumford <harry_mumford@hotmail.co.uk>
2019-12-14 09:27:52 +0000

    attempt to save state by running api using sinatra

--- public/lib/interface.js
+++ public/lib/interface.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
   })
 
   function updateTemperature() {
-    $('#temperature').text(thermostat.temperature);
+    $('#temperature').text(thermostat.temperature + '°');
     $('#temperature').attr('class', thermostat.energyUsage());
   }
 });
